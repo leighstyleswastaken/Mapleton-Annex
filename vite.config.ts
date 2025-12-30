9406acc6639a4bc2865482398013cea09101d555
leighstyleswastaken <leighstyleswastaken@gmail.com>
2025-12-30 19:59:24 +0000

    Update vite.config.ts
    add base

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      base: '/mapleton-annex/',   
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
